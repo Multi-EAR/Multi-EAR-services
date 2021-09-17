ee6099cdf976b7e5b2953b45706bb746d7b6a33a
Pieter Smets <mail@pietersmets.be>
2021-09-17 15:30:52 +0200

    status Flask html template

--- services/info/info.py
+++ services/info/info.py
@@ -7,19 +7,21 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return "Hello uWSGI from python version: <br>" + version
 
 
-@app.route("/status")
+@app.route('/status')
 def status():
     output = os.popen('/usr/bin/systemctl status multi-ear-uart').read().strip()
     output = "<br>".join(output.split("\n"))
-    return "service multi-ear-uart status: <br><br>" + output
+ 
+   return "service multi-ear-uart status: <br><br>" + output
+
+@app.route('/test')
+    return render_template('index.html', my_string='Jinja works!')
 
 
 if __name__ == "__main__":
-    app.run()
-
-# application = app
+    app.run(debug=True)
