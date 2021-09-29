bfb4dd785e70dd19fb25cec3c4cd054729b0488a
Pieter Smets <mail@pietersmets.be>
2021-09-29 17:21:09 +0200

    wifi

--- multi-ear-services/wifi/disable_wifi_access_point.sh
+++ multi-ear-services/wifi/disable_wifi_access_point.sh
@@ -2,10 +2,14 @@
 
 echo "$0"
 
+# stop and disable the access point services
 sudo systemctl daemon-reload
 sudo systemctl disable hostapd
 sudo systemctl stop hostapd
 sudo systemctl disable dnsmasq
 sudo systemctl stop dnsmasq
+
+# link to enable
+sudo ln -sf /opt/multi-ear-services/enable_wifi_access_point.sh switch_wifi_access_point_mode
 
 exit 0
--- multi-ear-services/wifi/enable_wifi_access_point.sh
+++ multi-ear-services/wifi/enable_wifi_access_point.sh
@@ -2,10 +2,14 @@
 
 echo "$0"
 
+# stop and disable the access point services
 sudo systemctl daemon-reload
 sudo systemctl enable hostapd
 sudo systemctl start hostapd
 sudo systemctl enable dnsmasq
 sudo systemctl start dnsmasq
+
+# link to disable
+sudo ln -sf /opt/multi-ear-services/disable_wifi_access_point.sh switch_wifi_access_point_mode
 
 exit 0
